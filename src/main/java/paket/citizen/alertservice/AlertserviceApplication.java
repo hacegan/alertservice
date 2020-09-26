4cdff5173921fd72e5b90da092b0939dc2436cde
Samet Dağ <samet822@hotmail.com>
2020-09-26 13:25:55 +0300

    Initial commit

---
+++ src/main/java/paket/citizen/alertservice/AlertserviceApplication.java
@@ -0,0 +1,13 @@
+package paket.citizen.alertservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/paket/citizen/alertservice/AlertserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package paket.citizen.alertservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
